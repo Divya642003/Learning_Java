f968656031ddc0c6ea6a9f3ff5561f453ed5f0ec
Divya642003 <divyasingh.300246@gmail.com>
2022-04-01 14:28:56 +0530

    commit

---
+++ Java_Practice/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,50 @@
+package Unit_01;
+
+
+
+/* 
+ * total 50 keywords
+ * 48 are used
+ * 2 are for future scope
+ * those 2 are static*,goto*
+ * 
+ * so for making static variable we use final keyword in java
+ * 
+ */
+
+
+public class P2_Task01_KeywordsInJava {
+
+	public static void main(String[] args) {
+		// variable name
+	//Syntax error on token "double", invalid VariableDeclaratorId
+		int double=12;
+		
+		//function name
+  //Syntax error on token "new", invalid AnnotationName		
+		int new() {
+			System.out.println();
+			return 0;
+		}
+		
+		//class name[given below]
+
+		//object name can never be any keywords in java 
+		ABC int=new ABC();
+	}
+   
+	//class name can never be any keywords in java
+	class int{
+		void display() {
+			System.out.println("Another class");
+		}
+	}
+	
+class ABC
+{
+		void display() {
+			System.out.println("Another class");
+		}
+	
+}
+
---
+++ Java_Practice/src/Unit_01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,52 @@
+package Unit_01;
+
+/*  
+ * piece of data/code that would never change
+ * constants never change once a value is assigned
+ * 
+ * private : "inside class only"
+ * protected :"class + same package"
+ * public : " class + within the package + outside the package"
+ * default : " no keywords , package-private {not outside package}"
+ */
+
+public class P2_Task02_ConstantsInJava 
+{
+
+		public static final double Pi=3.14;
+		//Pi=3.18;
+	  
+		public static void main(String[] args) 
+		{
+			System.out.println(Pi);
+			main("divksha");
+			
+			NewClass obj=new NewClass();
+			obj.add(1,2);
+			System.out.println(obj.Pi);
+		}
+		
+		
+		public static void main(String args) 
+		{
+			System.out.println(Pi);
+			
+		}
+		
+		void add(int a,int b) 
+		{
+			System.out.println(a + b);
+			System.out.println(Pi);
+		}
+}
+
+
+class NewClass
+{
+		 final double Pi=3.14;
+		 
+		 void add(int a,int b) {
+				System.out.println(a + b);
+				System.out.println(P2_Task02_ConstantsInJava.Pi);
+			}
+}
